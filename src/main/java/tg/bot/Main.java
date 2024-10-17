c8835ae03a2ba4c51ba5af0b7d53806d523b77b9
wushuo <2725730722@qq.com>
2024-10-17 10:12:50 +0800

    init

--- src/main/java/tg/bot/Main.java
+++ src/main/java/tg/bot/Main.java
@@ -1,9 +1,13 @@
 package tg.bot;
 
+import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.http.HttpUtil;
 import lombok.extern.slf4j.Slf4j;
 import tg.bot.action.BotAction;
 import tg.bot.util.ConfigUtil;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 public class Main {
@@ -16,8 +20,19 @@ public class Main {
             System.exit(1);
         }
 
+        int port = 13891;
+        List<String> list = Arrays.asList(ObjectUtil.defaultIfNull(args, new String[]{}));
+        for (int i = 0; i < list.size(); i++) {
+            String s = list.get(i);
+            if (s.equals("--port")) {
+                port = Integer.parseInt(list.get(i + 1));
+            }
+        }
+
+        log.info("port: {}", port);
+
         BotAction botAction = new BotAction();
-        HttpUtil.createServer(13891)
+        HttpUtil.createServer(port)
                 .addAction("/webhook", (req, res) -> {
                     try {
                         botAction.doAction(req, res);
